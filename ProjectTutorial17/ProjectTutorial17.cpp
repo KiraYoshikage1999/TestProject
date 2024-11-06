8f995e2d183dd0992f78fe1a5f67ce750f1ab476
Marah_gl87 <Marah_gl87@21-1>
2024-11-06 18:33:14 +0200

    Add project files.

---
+++ ProjectTutorial17/ProjectTutorial17.cpp
@@ -0,0 +1,15 @@
+// ProjectTutorial17.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <ctime>
+#include <string.h>
+
+
+using namespace std;
+
+int main()
+{
+    
+}
+
